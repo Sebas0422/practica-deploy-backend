c4c6e693b7a70ef1d354ac41e86f56f8adcd1af4
Sebastian Castedo <sebastian.castedoribera,03@gmail.com>
2025-05-15 18:10:09 -0400

    Test deploy

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.use(
   '/images',
   cors({
